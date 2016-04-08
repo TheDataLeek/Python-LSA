68a316ee7cb1ece9bbdab94d63603824cb2b6aad
William Farmer <willzfarmer@gmail.com>
2016-04-08 10:27:41 -0600

    Added logging for debug help

--- LSA.py
+++ LSA.py
@@ -14,6 +14,7 @@ import math
 import time
 import argparse
 import concurrent.futures
+import logging
 import typing
 import numpy as np
 import scipy.io as scio
@@ -25,20 +26,31 @@ import enforce
 
 
 def main():
+    logging.basicConfig(filename='LSA.log', level=logging.DEBUG)
+
+    logging.info('Program Start')
     """ Manage Execution """
     args = get_args()
 
+    logging.info('Program Arguments: {}'.format(str(args)))
+
     documents, doccount = open_documents(args.filename, args.count)
     print('Program Start. Loaded Data. Time Elapsed: {}\n'.format(time.clock()))
+    logging.info('Loaded Data. Time Elapsed: {}'.format(time.clock()))
 
     words = get_unique_words(documents, args.workers)
     wordcount = len(words.keys())
     topwords = ','.join([w for w, s in sorted(words.items(),
-                                              key=lambda tup: -tup[1]['freq'])[:100]])
+                                              key=lambda tup: -tup[1]['freq'])[:20]])
+
+    logging.info('Found Word Frequencies')
+    logging.info('{} Documents (m) by {} Unique Words (n)'.format(doccount, wordcount))
+    logging.info('Top 20 Most Frequent Words:{}'.format(topwords))
+    logging.info('Time Elapsed: {}'.format(time.clock()))
 
     print(('Found Word Frequencies\n'
            '\n{} Documents (m) by {} Unique Words (n)\n\n'
-           'Top 100 Most Frequent Words:{}\n'
+           'Top 20 Most Frequent Words:{}\n'
            'Time Elapsed: {}\n').format(doccount,
                                         wordcount,
                                         topwords,
@@ -46,9 +58,11 @@ def main():
 
     docmatrix, documents = get_sparse_matrix(documents, words, args.workers)
     print('Calculated Sparse Matrix\nTime Elapsed: {}\n'.format(time.clock()))
+    logging.info('Calculated Sparse Matrix. Time Elapsed: {}'.format(time.clock()))
 
     u, s, vt = ssl.svds(docmatrix.T, k=args.svdk)
     print('Calculated SVD Decomposition\nTime Elapsed: {}'.format(time.clock()))
+    logging.info('Calculated SVD Decomposition. Time Elapsed: {}'.format(time.clock()))
 
     if args.save:
         output = {'u':u, 'd': np.diag(s), 'vt':vt,
@@ -227,7 +241,7 @@ def get_args() -> argparse.Namespace:
                         help=('Number of documents to use from original set'))
     parser.add_argument('-k', '--svdk', type=int, default=20,
                         help=('SVD Degree'))
-    parser.add_argument('-f', '--filename', type=str, default='./data/jeopardy.csv',
+    parser.add_argument('-f', '--filename', type=str, default='./data/jeopardy/jeopardy.csv',
                         help=('File to use for analysis'))
     parser.add_argument('-s', '--save', action='store_true', default=False,
                         help=('Save output in .mat file.'))
