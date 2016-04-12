628cd994e5d94ea12dff772b43ba09d580eb54a2
William Farmer <willzfarmer@gmail.com>
2016-04-12 11:53:28 -0600

    Updated requirements

--- tests/test_scribe.py
+++ tests/test_scribe.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3.5
 
-import numpy as np
 import pytest
-
-from scribe.LSA.LSA import clean_text
-from scribe.LSA.LSA import unique_words
-from scribe.LSA.LSA import read_raw_docs
-from scribe.LSA.LSA import parse_docs
+from scribe.LSA import LSA
 
 WORKERS = 8
 
@@ -15,7 +10,7 @@ class TestLSA(object):
     @pytest.fixture(scope='module')
     def docs(self):
         contents = 'foo\nbar biz boo\nbaz foo bar\nfoo bar'
-        documents = read_raw_docs(contents.split('\n'), -1)
+        documents = LSA.read_raw_docs(contents.split('\n'), -1)
         return documents
 
     @pytest.fixture(scope='module')
@@ -23,22 +18,22 @@ class TestLSA(object):
         return len(docs)
 
     def test_cleaner(self):
-        assert clean_text('foo') == 'foo'
-        assert clean_text('~!f*&%o)!)!o{":?>') == 'foo'
-        assert clean_text('3240873245872345087foo123981304897345') == 'foo'
-        assert clean_text('foo bar') == 'foo bar'
-        assert clean_text('f234872034oo b3890ar0120') == 'foo bar'
-        assert clean_text('!@#():foo b&$:ar*&@%') == 'foo bar'
+        assert LSA.clean_text('foo') == 'foo'
+        assert LSA.clean_text('~!f*&%o)!)!o{":?>') == 'foo'
+        assert LSA.clean_text('3240873245872345087foo123981304897345') == 'foo'
+        assert LSA.clean_text('foo bar') == 'foo bar'
+        assert LSA.clean_text('f234872034oo b3890ar0120') == 'foo bar'
+        assert LSA.clean_text('!@#():foo b&$:ar*&@%') == 'foo bar'
 
     def test_clean_text_word(self):
         word = '!@#*(&F)(O&!{}:"><ObAR1230875'
-        assert clean_text(word) == 'foobar'
+        assert LSA.clean_text(word) == 'foobar'
 
     def test_clean_text_sentence(self):
         sentence = '!@#*(&F) (O&!{}:"><O bAR123 testfoo0875'
-        assert clean_text(sentence) == 'f oo bar testfoo'
+        assert LSA.clean_text(sentence) == 'f oo bar testfoo'
 
     def test_unique_words(self, docs):
-        unique_words_result = unique_words(docs)
+        unique_words_result = LSA.unique_words(docs)
         words = ['bar', 'baz', 'biz', 'boo', 'foo']
         assert sorted(unique_words_result.keys()) == words
