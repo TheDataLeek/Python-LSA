6673e41b5af6bc22f73954bf0f119f2b725a00ff
rrish <rishabh.raghavendran@colorado.edu>
2016-04-20 22:09:14 -0600

    Created a new file in which I'm doing the cosine similarity using scipy and also including spearmans rank correlation for doc comparisons. This is being done is a pseudo switch case. Will eventually change this into a nice try: error: type code block

---
+++ scribe/LSA/LSA_spearmans.py
@@ -0,0 +1,307 @@
+#!/usr/bin/env python3
+
+"""
+Library for (level 2 optimized) Latent Sentiment Analysis
+"""
+
+
+import re
+import math
+import concurrent.futures
+import typing
+import numpy as np
+from scipy.sparse import dok_matrix
+from scipy.sparse import dok
+from scipy import spatial
+import scipy.sparse.linalg as ssl
+import scipy.stats as sct
+from tqdm import tqdm
+import time
+import enforce
+from nltk.stem import SnowballStemmer
+from nltk.tokenize import TreebankWordTokenizer
+
+
+def analyze(filename: str, workers: int, count: int, svdk: int, save: bool) -> None:
+    """
+    Manage analysis of document set
+    """
+    documents, doccount = open_documents(filename, count)
+    print('Program Start. Loaded Data. Time Elapsed: {}\n'.format(time.clock()))
+
+    words = get_unique_words(documents, workers)
+    wordcount = len(words.keys())
+    topwords = ','.join([w for w, s in sorted(words.items(),
+                                              key=lambda tup: -tup[1]['freq'])[:20]])
+
+    print(('Found Word Frequencies\n'
+           '\n{} Documents (m) by {} Unique Words (n)\n\n'
+           'Top 20 Most Frequent Words:{}\n'
+           'Time Elapsed: {}\n').format(doccount,
+                                        wordcount,
+                                        topwords,
+                                        time.clock()))
+
+    docmatrix, documents = get_sparse_matrix(documents, words, workers)
+    print('Calculated Sparse Matrix\nTime Elapsed: {}\n'.format(time.clock()))
+
+    u, s, vt = decomposition(docmatrix, svdk)
+    print('Calculated SVD Decomposition\nTime Elapsed: {}'.format(time.clock()))
+
+    while True:
+        try:
+            selection = input('(w)ords or (d)ocuments? ').lower()
+            if selection == 'w':
+                matrix_comparison(u, s, vt, words, documents)
+            elif selection == 'd':
+                doc_comparisons(u, s, vt, documents)
+            elif selection == 'exit':
+                break
+        except KeyboardInterrupt:
+            break
+
+
+def decomposition(docmatrix, k):
+    u, s, vt = ssl.svds(docmatrix.T, k=k)
+    return u, s, vt
+
+
+def matrix_comparison(u, s, vt, words, documents):
+    wordlist = np.array(list(sorted(words.keys())), dtype=object)
+    word_indices = {w:i for i, w in enumerate(wordlist)}
+    d = np.diag(s)
+
+    num_docs = vt.shape[1]
+    num_words = u.shape[0]
+
+    word_mat = u @ d    # python 3.5 syntax for dot product
+    doc_mat = d @ vt
+
+    tokenizer = TreebankWordTokenizer()
+    stemmer = SnowballStemmer('english')
+    query = [w for w in clean_text(input('Enter the query: '), tokenizer, stemmer).split(' ') if w != '']
+
+    indices = []
+    error = False
+    for word in query:
+        try:
+            indices.append(word_indices[word])
+        except KeyError:
+            print('Invalid Word: {} -- Not in Corpus'.format(word))
+            error = True
+            break
+
+    if not error:
+        q = np.mean([word_mat[index] for index in indices], axis=0)
+
+        rank = np.zeros(num_docs)  # pre-initialize array for speed
+        for i in range(num_docs):
+            rank[i] = 1 - spatial.distance.cosine(doc_mat[:,i], q)
+
+        r = sorted(range(len(rank)), key=lambda x: rank[x])
+        print('\n')
+        for i in range(-1, -5, -1):
+            print(documents[r[i]])
+
+
+def doc_comparisons(u, s, vt, documents):
+    d = np.diag(s)
+    num_docs = vt.shape[1]
+    doc_mat = d @ vt
+
+    error = False
+
+    try:
+        query_str = 'Enter the document number you wish to query (between 0 and {} inclusive): '
+        index = int(input(query_str.format(len(documents) - 1)))
+        if index >= len(documents) or index < 0:
+            error = True
+        print('You Queried: {}'.format(documents[index]))
+    except ValueError:
+        print("Insert Valid Number")
+        error = True
+
+    if not error:
+        q = doc_mat[:, index]
+
+        rank = np.zeros(num_docs)
+        method = int(input("Enter 1 for Spearmans and 2 for Cosine Similarity")
+        if(method == 1):
+            for i in range(num_docs):
+                rank[i] = sct.spearmanr(doc_mat[:,i], q)[0]
+        elif(method == 2):
+            for i in range(num_docs):
+                rank[i] = 1 - spatial.distance.cosine(doc_mat[:,i], q)
+
+        r = sorted(range(len(rank)), key=lambda x: rank[x])
+        print('\n')
+        for i in range(-1, -5, -1):
+            print(documents[r[i]])
+
+
+@enforce.runtime_validation
+def open_documents(filename: str, size: int) -> typing.Tuple[np.ndarray, int]:
+    with open(filename, 'r') as datafile:
+        lines = datafile.read().split('\n')
+        documents = read_raw_docs(lines, size)
+    doccount = len(documents)
+    return documents, doccount
+
+
+@enforce.runtime_validation
+def read_raw_docs(lines: list, size: int) -> np.ndarray:
+    """
+    TODO: Parallelize tokenizing
+    """
+    if size == -1:
+        size = len(lines)
+    documents = np.empty(size, dtype=object)
+    tokenizer = TreebankWordTokenizer()
+    stemmer = SnowballStemmer('english')
+    for i, line in tqdm(enumerate(lines), total=size, leave=True, desc='Tokenizing Documents'):
+        if i >= size:
+            break
+        documents[i] = str(clean_text(line, tokenizer, stemmer))
+    return documents
+
+
+def clean_text(line: str, tokenizer: TreebankWordTokenizer, stemmer: SnowballStemmer) -> str:
+    """
+    This module is responsible for converting a document into a cleaned version using nltk
+    1. Tokenize using TreeBankTokenizer: http://www.nltk.org/api/nltk.tokenize.html#module-nltk.tokenize.treebank
+    2. Remove punctuation
+    3. Remove words that are 1-3 characters long
+    4. Stem using Snowball stemmer: http://www.nltk.org/howto/stem.html
+    5. Join back together
+    """
+    tokens = tokenizer.tokenize(line.lower())
+    stemmed_tokens = [stemmer.stem(w) for w in
+                      [re.sub('[^a-z0-9 ]+', '', word) for word in tokens] if len(w) > 3]
+    return ' '.join(stemmed_tokens)
+
+
+def clean_words(line: str) -> typing.Generator:
+    for word in clean_text(line).split(' '):
+        if word != '':
+            yield word
+
+
+@enforce.runtime_validation
+def get_unique_words(documents: np.ndarray, workers: int) -> dict:
+    """
+    Parallelize Unique Word Calculation
+
+    :documents: list of document strings
+    :workers: number of workers
+
+    :return: dictionary of word frequencies
+    """
+    data_bins = np.array_split(documents, workers)
+    wordlist = {}
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        futures = {executor.submit(unique_words, data_bins[i]):i for i in range(workers)}
+        for future in tqdm(concurrent.futures.as_completed(futures),
+                           desc='Determining Unique Words', leave=True, total=workers):
+            for word, stats in future.result().items():
+                try:
+                    wordlist[word]['freq'] += stats['freq']
+                    wordlist[word]['doccount'] += stats['doccount']
+                except KeyError:
+                    wordlist[word] = {'freq':stats['freq'], 'doccount':stats['doccount']}
+    return wordlist
+
+
+@enforce.runtime_validation
+def unique_words(data: np.ndarray) -> dict:
+    """
+    Finds unique word frequencies in documents
+
+    :data: list of document strings
+
+    :return: dictionary of word frequencies
+    """
+    words  = {}
+    olddoc = None
+    for doc in data:
+        for word in doc.split(' '):
+            if word != '':
+                try:
+                    words[word]['freq'] += 1
+                    if doc != olddoc:
+                        words[word]['doccount'] += 1
+                except KeyError:
+                    words[word] = {'freq': 1, 'doccount': 1}
+        olddoc = doc
+    return words
+
+
+def weight(total_doc_count: int, doccount: int, wordfreq: int) -> float:
+    """
+    Weighting function for Document Term Matrix.
+
+    tf-idf => https://en.wikipedia.org/wiki/Tf%E2%80%93idf
+    """
+    return math.log(total_doc_count / doccount) * wordfreq
+
+
+@enforce.runtime_validation
+def get_sparse_matrix(documents: np.ndarray, words: dict, workers: int, weighting: typing.Any=weight) -> typing.Tuple[dok.dok_matrix, np.ndarray]:
+    """
+    Parallelize Sparse Matrix Calculation
+
+    :documents: list of document strings
+    :words: dictionary of word frequencies
+    :workers: number of workers
+
+    :return: Sparse document term matrix
+    """
+    m         = len(documents)
+    n         = len(words.keys())
+    # Make sure we don't have more bins than workers
+    workers   = m if m < workers else workers
+    data_bins = np.array_split(documents, workers)
+    docmatrix = dok_matrix((m, n), dtype=float)
+    new_docs  = np.empty(len(documents), dtype=object)
+    offsets   = [len(data_bin) for data_bin in data_bins]
+    coffset   = 0
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        futures = {executor.submit(parse_docs, data_bins[i], words, len(documents), weight):i
+                   for i in range(workers)}
+        for future in tqdm(concurrent.futures.as_completed(futures),
+                           desc='Parsing Documents and Combining Arrays',
+                           leave=True, total=workers):
+            binnum = futures[future]
+            # Because order is not preserved in threads, we need to make sure we add
+            # the documents back in the correct order.
+            for i, doc in enumerate(data_bins[binnum]):
+                new_docs[coffset + i] = doc
+            # THIS IS THE BOTTLENECK
+            for key, value in future.result().items():
+                docmatrix[key[0] + coffset, key[1]] = value
+            coffset += offsets[binnum]
+    return docmatrix, new_docs
+
+
+@enforce.runtime_validation
+def parse_docs(data: np.ndarray, words: dict, doc_count: int, weight_func: typing.Any) -> dict:
+    """
+    Parallelize Sparse Matrix Calculation
+
+    :data: list of document strings
+    :words: dictionary of word frequencies
+    :total_doc_count: total number of documents (for tf-idf)
+    :weight_func: weighting function for code
+
+    :return: Basically sparse array with weighted values
+    """
+    m = len(data)
+    n = len(words.keys())
+    docmatrix = {}
+    wordref = {w:i for i, w in enumerate(sorted(words.keys()))}
+    for i, doc in enumerate(data):
+        for word in list(set(doc.split(' '))):
+            if word != '':
+                docmatrix[(i, wordref[word])] = weight_func(doc_count,
+                                                            words[word]['doccount'],
+                                                            words[word]['freq'])
+    return docmatrix
